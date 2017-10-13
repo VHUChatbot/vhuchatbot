f2558c7ef98a585328df8ceb23553d56bf7e4300
vhuchatbot <itc.vhu@gmail.com>
2017-10-14 01:37:58 +0700

    fist

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$DBHOST = "us-cdbr-iron-east-05.cleardb.net";
+$DBUSER = "bb615a63828210";
+$DBNAME = "heroku_ac1e5be76abebb9";
+$DBPW =  "78621080"; // Set your database password here
+$BOT_ID = "59e0f0b0e4b0662e5b005395";
+$TOKEN = "FA50ZrR0d2hGxpRBQYImVM6psoUdMYE4zgaPwmRPjuDGhCJ7TU1QjB3uDuUMXXnl";
+$BLOCK_NAME = "VHU_chat";
+$JSON = "VHU_text";
+?>
